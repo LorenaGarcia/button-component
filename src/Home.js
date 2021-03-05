c003087d9a071bf060a8d917dc0861d961b114fe
LorenaGarcia <lorenagarciar34@gmail.com>
2021-03-05 11:54:32 -0600

    github pages

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,13 @@
 import React from "react";
-import styled from "styled-components";
-import Menu from "./components/NavigationMenu";
+import Layout from "./components/Layout";
 import MainContent from "./components/MainContent";
 
 const Home = () => {
   return (
-    <Container>
-      <Navigation>
-        <Menu />
-      </Navigation>
-      <Main>
-        <MainContent />
-      </Main>
-    </Container>
+    <Layout>
+      <MainContent />
+    </Layout>
   );
 };
 
 export default Home;
-
-export const Container = styled.div`
-  display: grid;
-  height: 100vh;
-  grid-template-rows: 1fr;
-  grid-template-columns: 20% 1fr;
-  grid-template-areas: "navigation main";
-
-  @media (max-width: 480px) {
-    grid-template-columns: 1fr;
-    grid-template-areas: "main";
-  }
-`;
-
-export const Navigation = styled.div`
-  grid-area: navigation;
-`;
-
-export const Main = styled.div`
-  grid-area: main;
-  padding: 3rem;
-`;
